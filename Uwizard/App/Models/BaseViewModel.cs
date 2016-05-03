11c0427500f9144e3e89e90b5b759cad299439b0
EvilGamerX <evilgamerx42@gmail.com>
2016-05-03 19:55:38 -0400

    Adding viewmodels to work with independent views

---
+++ Uwizard/App/Models/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+using Uwizard.Annotations;
+
+namespace Uwizard.App.Models
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public abstract Control View();
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Uwizard/App/Models/MainFormViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Uwizard.App.Models
+{
+    public class MainFormViewModel : BaseViewModel
+    {
+        public ICollection<BaseViewModel> Views { get; set; }
+
+        public MainFormViewModel()
+        {
+            Views = new List<BaseViewModel>();
+        }
+
+        public override Control View()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
