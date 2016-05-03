d91185c60d9595ab34a9b1b5f353456a1ce9d8c2
EvilGamerX <evilgamerx42@gmail.com>
2016-05-03 19:52:41 -0400

    Made WiiUDisk and Point3D their own classes

---
+++ Uwizard/Entities/WiiUDisk.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Serialization;
+
+namespace Uwizard.Entities
+{
+    public class WiiUDisk
+    {
+        public string Id;
+        public string Name;
+        public string Keyhash;
+        public string Key;
+        public string Description;
+        public RegionCodeEnum RegionCode;
+        public string FilePath;
+    }
+}
