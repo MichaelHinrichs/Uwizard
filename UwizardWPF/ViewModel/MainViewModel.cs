1f70216e120aae091768b0255191c827bdefab3b
EvilGamerX <evilgamerx42@gmail.com>
2016-05-09 06:29:01 -0400

    Initial Commit of WPF project

---
+++ UwizardWPF/ViewModel/MainViewModel.cs
@@ -0,0 +1,41 @@
+using GalaSoft.MvvmLight;
+
+namespace UwizardWPF.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        private bool _enabled;
+
+        public bool Enabled
+        {
+            get { return _enabled;}
+            set { Set(ref _enabled, value); }
+        }
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+            ////if (IsInDesignMode)
+            ////{
+            ////    // Code runs in Blend --> create design time data.
+            ////}
+            ////else
+            ////{
+            ////    // Code runs "for real"
+            ////}
+        }
+    }
+}
