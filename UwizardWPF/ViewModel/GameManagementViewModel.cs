0998b0bdb1d555c07a8de2effd7ed513965312a4
EvilGamerX <evilgamerx42@gmail.com>
2016-05-12 09:29:33 -0400

    Created Two new ViewModels, and Started on View For Management

---
+++ UwizardWPF/ViewModel/GameManagementViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace UwizardWPF.ViewModel
+{
+    public class GameManagementViewModel : TabViewModel
+    {
+        public string Hi => "Hello There";
+
+        public GameManagementViewModel(string header) : base(header)
+        {
+        }
+    }
+}
---
+++ UwizardWPF/ViewModel/TabViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using GalaSoft.MvvmLight;
+
+namespace UwizardWPF.ViewModel
+{
+    public class TabViewModel : ViewModelBase
+    {
+        public string Header { get; }
+
+        protected TabViewModel(string header)
+        {
+            Header = header;
+        }
+    }
+}
