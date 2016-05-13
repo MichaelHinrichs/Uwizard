e047e9108fdbecfa68f3dbe4d8acde89bd5764dc
STEPH GASPAR <sg171@nyu.edu>
2016-05-13 11:48:10 -0400

    Split EncodeDecode into Encode and Decode

--- UwizardWPF/Extensions/StringExtensions.cs
+++ UwizardWPF/Extensions/StringExtensions.cs
@@ -1,21 +1,20 @@
 ﻿using System;
 using System.Linq;
-using Microsoft.VisualBasic;
 
 namespace UwizardWPF.Extensions
 {
     public static class StringExtensions
     {
-        public static string EncodeDecode(this string text)
+        public static string Encode(this string text)
         {
-            var ret = "";
-            for (var c = 0; c < text.Length; c++)
-            {
-                var ch = (byte)(255 - Strings.Asc(text[c]));
-                if (c > 0) ch = (byte)(ch ^ (c & 255));
-                ret = ret + Strings.Chr(ch);
-            }
-            return ret;
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(text);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string Decode(this string text)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(text);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
         }
 
         public static byte[] ToByteArray(this string hex)
