779d6a33549a13b13630ebcadebbc90d3d68f591
STEPH GASPAR <sg171@nyu.edu>
2016-05-23 13:41:20 -0400

    Updated IContainer and implemented on UwizardContainer

--- UwizardWPF/MVVM/IContainer.cs
+++ UwizardWPF/MVVM/IContainer.cs
@@ -1,4 +1,12 @@
-﻿namespace UwizardWPF.MVVM
+﻿using System;
+using System.Collections.Generic;
+
+namespace UwizardWPF.MVVM
 {
-    public interface IContainer { }
+    public interface IContainer {
+        T GetInstance<T>() where T : class;
+        object GetInstance(Type type);
+        IEnumerable<T> GetAllInstances<T>() where T : class;
+        IEnumerable<object> GetAllInstances(Type type);
+    }
 }
--- UwizardWPF/MVVM/UwizardContainer.cs
+++ UwizardWPF/MVVM/UwizardContainer.cs
@@ -1,4 +1,6 @@
-﻿using SimpleInjector;
+﻿using System;
+using System.Collections.Generic;
+using SimpleInjector;
 
 namespace UwizardWPF.MVVM
 {
@@ -6,6 +8,29 @@ namespace UwizardWPF.MVVM
     {
         private Container _container;
 
-        public
+        public UwizardContainer(Container container)
+        {
+            _container = container;
+        }
+
+        public T GetInstance<T>() where T : class
+        {
+            return _container.GetInstance<T>();
+        }
+
+        public object GetInstance(Type type)
+        {
+            return _container.GetInstance(type);
+        }
+
+        public IEnumerable<T> GetAllInstances<T>() where T : class
+        {
+            return _container.GetAllInstances<T>();
+        }
+
+        public IEnumerable<object> GetAllInstances(Type type)
+        {
+            return _container.GetAllInstances(type);
+        }
     }
 }
